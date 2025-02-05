f34aa9555af6090a0fd8bd1a827d9ca62b6a6cb6
Martin Smedmark <martin.smedmark@me.com>
2025-02-05 15:45:46 +0100

    remove use of old objects group

--- src/managers/object-manager.ts
+++ src/managers/object-manager.ts
@@ -1,4 +1,3 @@
-import { objectsGroup } from "../old/objects";
 import * as THREE from "three";
 
 export class ObjectManager {
@@ -6,18 +5,51 @@ export class ObjectManager {
     THREE.Mesh,
     { x: number; y: number; z: number }
   >;
+  private donutGeometry = new THREE.TorusGeometry(4, 3, 250, 500);
+  private boxGeometry = new THREE.BoxGeometry(10, 10, 10);
+  private material = new THREE.MeshNormalMaterial();
+
+  public objectsGroup: THREE.Group = new THREE.Group();
 
   constructor() {
     this.objectRotationSpeeds = new Map();
   }
 
-  initialize(scene: THREE.Scene) {
+  initialize() {
+    this.initializeObjects();
     this.setupObjectRotation();
-    scene.add(objectsGroup);
+  }
+
+  private initializeObjects() {
+    for (let i = 0; i < 30; i++) {
+      const donut = new THREE.Mesh(this.donutGeometry, this.material);
+      this.randomizeAndAdd(donut);
+    }
+
+    for (let i = 0; i < 30; i++) {
+      const box = new THREE.Mesh(this.boxGeometry, this.material);
+      this.randomizeAndAdd(box);
+    }
+  }
+
+  private randomizeAndAdd(mesh: THREE.Mesh) {
+    mesh.position.x = (Math.random() - 0.5) * 350;
+    mesh.position.y = (Math.random() - 0.5) * 350;
+    mesh.position.z = Math.random() * -350 - 100;
+
+    mesh.rotation.x = Math.random() * Math.PI;
+    mesh.rotation.y = Math.random() * Math.PI;
+
+    const scale = 0.3 + Math.random();
+    mesh.scale.x = scale;
+    mesh.scale.y = scale;
+    mesh.scale.z = scale;
+
+    this.objectsGroup.add(mesh);
   }
 
   private setupObjectRotation() {
-    objectsGroup.traverse((child) => {
+    this.objectsGroup.traverse((child) => {
       if (child instanceof THREE.Mesh) {
         this.objectRotationSpeeds.set(child, {
           x: (Math.random() - 0.5) * 2,
@@ -30,7 +62,7 @@ export class ObjectManager {
 
   render() {
     // Rotate all objects
-    objectsGroup.traverse((child) => {
+    this.objectsGroup.traverse((child) => {
       if (!(child instanceof THREE.Mesh)) return;
 
       const speed = this.objectRotationSpeeds.get(child);
